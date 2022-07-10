c435a847b0bea544bae4668f2e06d09b9172b8a7
natalia_obydova <serna.92@yandex.ru>
2022-07-10 21:13:34 +0400

    Task003

---
+++ sem1_09.07/003/Program.cs
@@ -0,0 +1,12 @@
+﻿// С клавиатуры вводятся два числа a и b. Найти максимальное из них.
+
+int a,b;
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+s=Console.ReadLine();
+b=Convert.ToInt32(s);
+int max=a;
+
+if (a>b) max=a;
+if (b>a) max=b;
+System.Console.WriteLine(max);
