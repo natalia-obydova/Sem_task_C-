9f87e7cecb9e376ed0a91476479bbdf34c760908
natalia_obydova <serna.92@yandex.ru>
2022-07-10 19:00:12 +0400

    Task001

---
+++ sem1_09.07/Ex001/Program.cs
@@ -0,0 +1,13 @@
+﻿//С клавиатуры вводится целое число. Вывести квадрат числа 
+int a=50;
+int b;
+string? s=Console.ReadLine();//ввод данных
+a=Convert.ToInt32(s);
+b=a*a;//обработка данных 
+System.Console.WriteLine($"{a}^2={b}");//строка интерполяции. вывод результата
+
+/*
+string? s;
+s=Console.ReadLine();
+System.Console.WriteLine(s);
+*/
