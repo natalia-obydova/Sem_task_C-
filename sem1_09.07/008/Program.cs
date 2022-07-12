daf1366d23edf1146864e8f126f80911a8bed976
natalia_obydova <serna.92@yandex.ru>
2022-07-12 23:29:06 +0400

    Task008

---
+++ sem1_09.07/008/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вывести на экран числа от -N до N
+
+string a;
+Console.Write("Введите число: ");
+a = Console.ReadLine();
+int n = Convert.ToInt32(a);
+int nn=-n;
+while(nn<=n)
+{
+    Console.Write(nn + " ");
+    nn++;
+}
