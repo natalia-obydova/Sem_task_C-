0ec50e4a491f253b660dbab221ce9c770e7777bf
natalia_obydova <serna.92@yandex.ru>
2022-07-12 19:49:02 +0400

    Task006_

--- sem1_09.07/006/Program.cs
+++ sem1_09.07/006/Program.cs
@@ -1,11 +1,11 @@
 ﻿// Написать программу вычисления значения функции y = sin(a). (Класс Math)
-/*
+
 double x, y;
 x = Math.PI; //константа пи
 y = Math.Sin(x);
 System.Console.WriteLine($"sin({x})={y}");
-*/
 
+/*
 //тип возвращаемого значения Имя_функции (аргументы функции)
 double Sin(double x)//double - тип возвращаемого значения, Sin - название функции, в скобках аргумент.
 {
@@ -16,5 +16,6 @@ void Pause()
     System.Console.WriteLine("Press any key");
 }
 
-double y-Sin(3.14);
+double y=Sin(3.14);
 System.Console.WriteLine(y);
+*/
