2ffa2e561be2bac6db59cc69418b32530b49dde6
natalia_obydova <serna.92@yandex.ru>
2022-07-12 23:17:31 +0400

    Task007

---
+++ sem1_09.07/007/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить является ли число чётным.
+
+string a;
+Console.Write("Введите число: ");
+a = Console.ReadLine();
+int n = Convert.ToInt32(a);
+if(n%2==0)
+{
+    System.Console.WriteLine("четное");
+}
+else
+{
+    System.Console.WriteLine("нечетное");
+}
