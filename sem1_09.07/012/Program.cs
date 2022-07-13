457396b9c9dccf10b921e8b3db0938325ffed91f
natalia_obydova <serna.92@yandex.ru>
2022-07-13 20:22:41 +0400

    Task012

---
+++ sem1_09.07/012/Program.cs
@@ -0,0 +1,16 @@
+﻿// С клавиатуры вводится целое число из  диапазона  [10, 99]. Показать наибольшую цифру числа
+
+System.Console.Write("Введите целое число от 10 до 99: ");
+string? a = Console.ReadLine();
+int b = Convert.ToInt32(a);
+if (b>=10 && b<=99)
+{
+    int c = b%10;
+    int d = b/10;
+    if (c>d) System.Console.WriteLine(c);
+    else System.Console.WriteLine(d);
+}
+else 
+{
+    System.Console.WriteLine("ALARM");
+}
