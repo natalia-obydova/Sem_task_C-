2f9c7e76e36d664f9532b57ca467ee85d74ba1fe
natalia_obydova <serna.92@yandex.ru>
2022-07-13 20:40:48 +0400

    Task013

---
+++ sem1_09.07/013/Program.cs
@@ -0,0 +1,8 @@
+﻿// Удалить вторую цифру целого числа введенного с клавиатуры.
+
+System.Console.Write("Введите целое число: ");
+string? a = Console.ReadLine();
+int b = Convert.ToInt32(a);
+int c = b%10;
+int d = b/100;
+System.Console.WriteLine(d*10+c);
