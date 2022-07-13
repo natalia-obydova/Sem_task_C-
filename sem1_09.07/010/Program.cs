e69e28e4aeb1a30c683aab492c042ebd9ed651a8
natalia_obydova <serna.92@yandex.ru>
2022-07-13 20:00:15 +0400

    Task010

---
+++ sem1_09.07/010/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вывести на экран последнюю цифру целого числа введенного с клавиатуры.
+
+System.Console.WriteLine("Введите целое число: ");
+string a=System.Console.ReadLine();
+int b=Convert.ToInt32(a);
+System.Console.WriteLine(b%10);
+
+
+
