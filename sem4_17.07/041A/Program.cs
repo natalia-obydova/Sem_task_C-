0433e6364eff0a0af96b5cb502e13f1d6ca36d6a
natalia_obydova <serna.92@yandex.ru>
2022-08-13 17:09:38 +0400

    041A_042A

---
+++ sem4_17.07/041A/Program.cs
@@ -0,0 +1,26 @@
+﻿// Дана квадратная матрица. Проверить, является ли она симметричной относительно главной диагонали.
+
+
+bool sim = true;
+int[,] a = new int[3, 3] { { 9, 1, 2 }, { 1, 7, 1 }, { 2, 1, 3 } };
+
+for (int i = 0; i < a.GetLength(0); i++)
+{
+    for (int j = 0; j < a.GetLength(1); j++) System.Console.Write($"{a[i, j]} "); 
+    System.Console.WriteLine();
+}
+
+for (int i = 0; i < a.GetLength(0); i++)
+{
+    for (int j = 0; j < a.GetLength(1); j++)
+    {
+        if (a[i, j] != a[j, i])
+        {
+            sim = false;
+            break;
+        }
+    }
+}
+if (sim) System.Console.WriteLine("массив симметричен");
+else System.Console.WriteLine("массив не симметричен");
+
---
+++ sem4_17.07/042A/Program.cs
@@ -0,0 +1,54 @@
+﻿/*Даны две квадратных таблицы чисел. Требуется построить третью,каждый элемент которой равен сумме элементов
+стоящих на том же месте в 1-й и 2-й таблицах. 
+Входные данные
+Во входном файле записано сначала число N, затем записана первая таблица,а после нее - вторая.
+Элементы таблиц - числа от 0 до 100. 100>=N>=1.
+*/
+
+System.Console.WriteLine("Введите размер квадратного массива: ");
+int N = Convert.ToInt32(System.Console.ReadLine());
+int[,] a1 = new int[N, N];
+int[,] a2 = new int[N, N];
+int[,] a3 = new int[N, N];
+
+Random random = new Random(); //создали объект типа Random
+//1 массив
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        a1[i, j] = random.Next(1, 11);
+        System.Console.Write($"{a1[i, j]} ");
+
+    }
+    System.Console.WriteLine();
+}
+System.Console.WriteLine();
+//2 массив
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        a2[i, j] = random.Next(1, 11);
+        System.Console.Write($"{a2[i, j]} ");
+
+    }
+    System.Console.WriteLine();
+}
+
+System.Console.WriteLine();
+
+//3 массив
+
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        a3[i, j] = a1 [i, j] + a2 [i, j]; 
+        System.Console.Write($"{a3[i, j]} ");
+       
+    }
+    System.Console.WriteLine();
+}
+
+
