ff2a17d00787ec47e0e680a5b250df32b8b6e64e
natalia_obydova <serna.92@yandex.ru>
2022-08-20 17:16:45 +0400

    054-056

---
+++ sem6_07.08/054/Program.cs
@@ -0,0 +1,63 @@
+﻿/*
+Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки 
+двумерного массива. Например задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+int[,] arr = new int[,]
+{ 
+{1, 4, 7, 2},
+{5, 9, 2, 3},
+{8, 4, 2, 4},
+};
+
+Print (arr);
+System.Console.WriteLine();
+Print(Sort(arr));
+
+
+int [,] Sort(int[,] arr)
+{
+    for(int i=0;i<arr.GetLength(0); i++)
+    {
+        for(int j=0; j<arr.GetLength(1)-1; j++)
+        { 
+            int max=0; 
+            int iMax=0; 
+            for(int k=j; k<arr.GetLength(1); k++) 
+            {
+                if (arr[i,k]>max)   
+                {
+                    max=arr[i,k]; 
+                    iMax=k;     
+                }        
+            }    
+            if (iMax!=j)
+            {
+                int P=arr[i,j];
+                arr[i,j]=arr[i,iMax]; 
+                arr[i,iMax]=P; 
+            }
+        } 
+    }
+    return arr;
+}
+
+
+void Print (int [,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++) 
+        {
+            System.Console.Write($"{arr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
---
+++ sem6_07.08/056/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей 
+суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+
+int[,] arr = new int[,]
+{
+{1, 4, 7, 2},
+{5, 9, 2, 3},
+{8, 4, 2, 4},
+{5, 2, 6, 7},
+};
+Print(arr);
+
+
+int Sum = 0, Sum1 = 0, Sum2 = 0, Row = 0;
+for (int i = 0; i < arr.GetLength(0); i++) //Проходим по циклу строк
+{
+    Sum2 = 0;
+    for (int j = 0; j < arr.GetLength(1); j++) //Проходим по циклу столбцов
+    {
+        Sum2 = Sum2 + arr[i, j]; //Сумма всех членов 1ой строки.
+    }
+    if (Sum2 < Sum1)
+    {
+        Sum = Sum2; Row = i;
+    }
+    Sum1 = Sum2;
+}
+System.Console.WriteLine();
+Console.WriteLine("{0} строка", Row);
+
+void Print(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write($"{arr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
